8c3795d96074c78c2da82a23d16f945115d4cb36
KangJeongHo <intkjh@naver.com>
2023-01-16 13:50:03 +0900

    23_1_16_강정호_1
    주유기 생성 작업중 중간업로드

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,31 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-}
+
+    private static GameManager instance = null;
+
+    void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+ 
+    }

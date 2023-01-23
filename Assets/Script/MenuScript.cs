46833cbf2da1c526fed39e2b53d0f367da3f33c5
doma1245 <123345037+doma1245@users.noreply.github.com>
2023-01-23 14:51:38 +0900

    23_1_23_노명보_1
    상점씬 제작중

--- Assets/Script/MenuScript.cs
+++ Assets/Script/MenuScript.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using Script;
 using Unity.VisualScripting;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace Script
 {
@@ -18,7 +19,7 @@ namespace Script
             Menu_Button.AddComponent<Menu>();
             if (GameDirector.MenuValue.Menu_Cancle_Panel == null)
             {
-                Debug.LogWarning("sdasda");
+                Debug.LogWarning("if (GameDirector.MenuValue.Menu_Cancle_Panel == null)");
             }
             GameDirector.MenuValue.Menu_Cancle_Panel.AddComponent<Menu_Cancle_Panel>();
 
@@ -26,9 +27,36 @@ namespace Script
     }
 }
 
+#region Menu_Sub
 
+enum MENU_LIST
+{
+    SETTING,
+    SHOP,
+}
+
+
+public class Menu_Shop : MonoBehaviour, IPointerClickHandler
+{
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("ShopScene");
+    }
+}
+public class Menu_Setting : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+}
+
+#endregion
 
 #region Menu
+
+
 
 public class Menu_Cancle_Panel : MonoBehaviour ,IPointerClickHandler
 {
@@ -40,23 +68,36 @@ public class Menu_Cancle_Panel : MonoBehaviour ,IPointerClickHandler
 }
 public class Menu : MonoBehaviour,IPointerClickHandler
 {
+    MENU_LIST m_Menu_List = MENU_LIST.SHOP;
+
     private GameObject InstObject;
 
     private Vector3 Reset_Pre_InstObject_Pos = Vector3.zero;
     private Vector3 Pre_InstObject_Pos = Vector3.zero;
     
     private int Menu_Count = 0;
-    private List<GameObject> Menu_List = null;
+    private List<GameObject> Exist_Menu = null;
     bool IsMenu_ClickAvailable = true;
 
+    void class_In_InstObject(MENU_LIST _List)
+    {
+        if(_List == MENU_LIST.SETTING)
+        {
+            InstObject.AddComponent<Menu_Setting>();
+        }
+        else if (_List == MENU_LIST.SHOP)
+        {
+            InstObject.AddComponent<Menu_Shop>();
+        }
+    }
 
     void Menu_Detroy()
     {
         if (true == GameDirector.MenuLogicValue.Is_Menu_Cancle)
         {
-            for (int i = 0; i < Menu_List.Count; i++)
+            for (int i = 0; i < Exist_Menu.Count; i++)
             {
-                Destroy(Menu_List[i]);
+                Destroy(Exist_Menu[i]);
             }
 
             Menu_Count = 0;
@@ -73,7 +114,7 @@ public class Menu : MonoBehaviour,IPointerClickHandler
     void Open_Menu_Create() //메뉴칸 만들고 마지막 메뉴칸에 곂치게
     {
         InstObject = GameObject.Instantiate(GameDirector.MenuValue.Button);
-        Menu_List.Add(InstObject);
+        Exist_Menu.Add(InstObject);
         InstObject.transform.SetParent(this.transform);
         InstObject.transform.localScale = Vector3.one;
         InstObject.transform.localPosition = Pre_InstObject_Pos;
@@ -83,22 +124,25 @@ public class Menu : MonoBehaviour,IPointerClickHandler
     IEnumerator Open_Menu() // 메뉴 누르면 나오게 , 아래로 천천히 
     {
         Open_Menu_Create();
+        
+        Vector3 Origin = InstObject.transform.localPosition;
 
-        Vector3 Origin = InstObject.transform.position;
-        while (Origin.y - GameDirector.MenuValue.Menu_Distance <= InstObject.transform.position.y)
+        while (Origin.y - GameDirector.MenuValue.Menu_Distance < InstObject.transform.localPosition.y)
         {
-            InstObject.transform.position += Vector3.down *Time.deltaTime * GameDirector.MenuValue.Menu_Down_Speed;
-            if (Origin.y - GameDirector.MenuValue.Menu_Distance > InstObject.transform.position.y)
+            InstObject.transform.localPosition += Vector3.down *Time.deltaTime * GameDirector.MenuValue.Menu_Down_Speed;
+            if (Origin.y - GameDirector.MenuValue.Menu_Distance > InstObject.transform.localPosition.y)
             {
-                InstObject.transform.position = Origin + Vector3.down * GameDirector.MenuValue.Menu_Distance;
+                InstObject.transform.localPosition = Origin + Vector3.down * GameDirector.MenuValue.Menu_Distance;
             }
             
             yield return null;
         }
 
+        class_In_InstObject(m_Menu_List);
+        m_Menu_List += 1;                // 다음 이넘으로 넘어가게 (Menu_Setting -> Menu_Shop -> ...)
         Menu_Count += 1;
         Pre_InstObject_Pos = InstObject.transform.localPosition;
-        Debug.Log(Menu_Count);
+
         if (Menu_Count < GameDirector.MenuValue.Menu_Count) // 메뉴 갯수만큼 반복
         {
             StartCoroutine(Open_Menu());
@@ -118,7 +162,7 @@ public class Menu : MonoBehaviour,IPointerClickHandler
 
     private void Awake()
     {
-        Menu_List = new List<GameObject>();
+        Exist_Menu = new List<GameObject>();
         Reset_Pre_InstObject_Pos = Pre_InstObject_Pos;
     }
 
